91a8daaf139bfd86fcede5c4f394da098e4b3bfb
Marc LeBrun <mlb1729@users.noreply.github.com>
2020-10-03 15:00:17 -0700

    Initial project skeleton

---
+++ src/main/java/com/seqhak/SeqhakApplication.java
@@ -0,0 +1,13 @@
+package com.seqhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeqhakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeqhakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seqhak/SeqhakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seqhak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeqhakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
